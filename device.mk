4e5210bc4ed18c5225390e31e08d4ffdd7fb85b3
Bruno Martins <bgcngm@gmail.com>
2024-05-18 13:21:42 +0100

    tulip: Use common libqti-perfd-client and power-libperfmgr
    Change-Id: I22d6b35c72629553817005deaefcae60abc72cfd
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -31,7 +31,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH) \
     hardware/google/interfaces \
-    hardware/google/pixel
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -394,7 +396,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.tulip-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     android.hardware.power.stats@1.0-service.mock \
     libqti-perfd-client
 
