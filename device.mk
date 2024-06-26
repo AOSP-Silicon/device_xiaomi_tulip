8d33756f407dcbe4d64c9204ef043d4e7856e6f7
GhostMaster69-dev <rathore6375@gmail.com>
2024-06-26 12:16:18 +0530

    tulip: Use the `speed` compiler filter for system server
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -412,7 +412,7 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full
 
 # Speed profile services and wifi-service to reduce RAM and storage
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed
 PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 USE_DEX2OAT_DEBUG := false
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
