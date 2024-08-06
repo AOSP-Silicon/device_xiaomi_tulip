ad38e0225f36d071781158ec126f0f59d0fb1e68
GhostMaster69-dev <rathore6375@gmail.com>
2024-08-07 01:42:16 +0530

    tulip: Build missing 32-bit camera lib for 14 QPR3
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5:64 \
     libdng_sdk.vendor \
+    libpng.vendor:32 \
     vendor.qti.hardware.camera.device@1.0:64
 
 # Cgroup and task_profiles
