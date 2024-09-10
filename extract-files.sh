a79b36542e9b2e67e1c1bf12a770ba47207740fe
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    tulip: Shim libwvhidl with libcrypto_shim
    The following change
    https://github.com/google/boringssl/commit/e202e51cb0912f36dafbd2e67cf04d6ec82f3180
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -84,6 +84,7 @@ function blob_fixup() {
             sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
             ;;
 	vendor/lib64/libwvhidl.so)
+            grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             "${PATCHELF}" --replace-needed libprotobuf-cpp-lite-3.9.1.so libprotobuf-cpp-full-3.9.1.so "${2}"
             ;;
 	vendor/bin/pm-service)
