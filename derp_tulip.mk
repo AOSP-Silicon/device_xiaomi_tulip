4eacd8dbd7eba61e827d8455533b172c42823d93
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    tulip: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- derp_tulip.mk
+++ derp_tulip.mk
@@ -32,14 +32,12 @@ TARGET_VENDOR_PRODUCT_NAME := tulip
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Override Product Name
-PRODUCT_SYSTEM_NAME := tulip
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
 # Override default Build Fingerprint and Description
-BUILD_FINGERPRINT := xiaomi/tulip/tulip:$(PLATFORM_VERSION)/$(BUILD_ID)/$(shell date -u +%s):user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_VERSION_TAGS=release-keys \
-    TARGET_BUILD_FLAVOR=tulip-user \
-    TARGET_BUILD_TYPE=user \
-    PRIVATE_BUILD_DESC="tulip-user $(PLATFORM_VERSION) $(BUILD_ID) $(shell date -u +%s) release-keys"
+    BuildDesc="tulip-user 9 PKQ1.180904.001 V12.0.1.0.PEKMIXM release-keys" \
+    BuildFingerprint="xiaomi/tulip/tulip:9/PKQ1.180904.001/V12.0.1.0.PEKMIXM:user/release-keys" \
+    BuildKeys=release-keys \
+    BuildFlavor=tulip-user \
+    BuildVariant=user \
+    DeviceProduct=tulip \
+    SystemName=tulip
