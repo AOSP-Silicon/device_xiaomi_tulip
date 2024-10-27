adf3432403976c8245dd8c2b8ddfed242252fb2b
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-27 16:57:40 -0400

    tulip: Ensure we build the vendor version of libcrypto_shim
    Reference: Ia42de59ff157cddc930a111f8ce5e36e645055ed
    Change-Id: I9c9340b37a1ad0d6c22d3947541fe1196a5efeec
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -210,6 +210,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
     android.hardware.drm@1.4.vendor \
     libcrypto_shim \
+    libcrypto_shim.vendor \
     libhidlmemory.vendor
 
 # Dynamic Partitions
