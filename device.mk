dfe02fdb326ba7cd52748d58e7ad4ec577f896ea
GhostMaster69-dev <rathore6375@gmail.com>
2024-12-21 21:45:27 +0530

    tulip: Build libmaxxaudio vendor lib and enable maxx_audio feature
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -87,6 +87,7 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
+    libmaxxaudio \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
