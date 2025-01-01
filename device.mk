6263976f566c7fa249e0c7a4edf3c9a0b541b508
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 04:12:35 +0200

    tulip: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Idb002fc81fca3321f70fb3bba83f4af44354ce81
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -179,7 +179,7 @@ PRODUCT_COPY_FILES += \
 # Display
 PRODUCT_PACKAGES += \
     gralloc.sdm660 \
-    hwcomposer.sdm660 \
+    hwcomposer.qcom \
     libtinyxml \
     libdisplayconfig \
     libqdMetaData
