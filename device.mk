24bcf049e6fbdcbb4143c2e526ec5845ec474dc8
GhostMaster69-dev <rathore6375@gmail.com>
2025-02-16 01:39:39 +0530

    tulip: Update xiaomi AIDL Power HAL from lineage
    source: https://github.com/LineageOS/android_hardware_xiaomi/tree/lineage-20/aidl/power-libperfmgr
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -394,7 +394,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power-service.tulip-libperfmgr \
     android.hardware.power.stats@1.0-service.mock \
     libqti-perfd-client
 
