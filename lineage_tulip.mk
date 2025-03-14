bef157e3c8a03922b1b316559bb5ddc4fa442596
GhostMaster69-dev <rathore6375@gmail.com>
2025-03-14 10:59:21 +0530

    tulip: Add-on Pixel Gapps
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- lineage_tulip.mk
+++ lineage_tulip.mk
@@ -20,6 +20,16 @@ BUILD_BCR := false
 # Pixel Gapps
 TARGET_USES_PICO_GAPPS := true
 
+ifeq ($(TARGET_USES_PICO_GAPPS),true)
+PRODUCT_PACKAGES += \
+    CalendarGooglePrebuilt \
+    FilesPrebuilt \
+    NgaResources \
+    Photos \
+    PrebuiltBugle \
+    Velvet
+endif
+
 # Device Info
 PRODUCT_NAME := lineage_tulip
 PRODUCT_DEVICE := tulip
